879f3759b13142cfcf0f1f961ceafb3fb37a366e
uros milenkovic <urosm4471@gmail.com>
2025-01-21 12:52:35 +0100

    commit4

--- routes/google/google.js
+++ routes/google/google.js
@@ -1,4 +1,4 @@
-const express = require("express");
+/*const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const jwt = require("jsonwebtoken");
@@ -79,6 +79,100 @@ google.get(
       process.env.FRONTEND_URL
     }/success?token=${encodeURIComponent(userToken)}`;
     res.redirect(redirectUrl);
+  }
+);
+
+module.exports = google;*/
+
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+const google = express.Router();
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const UsersModel = require("../../models/userModel/userModel");
+
+google.use(
+  session({
+    secret: process.env.GOOGLE_CLIENT_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+google.use(passport.initialize());
+google.use(passport.session());
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: process.env.GOOGLE_CALLBACK_URL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await UsersModel.findOne({ email: profile._json.email });
+        if (!user) {
+          const { _json: userData } = profile;
+          const userToSave = new UsersModel({
+            name: userData.given_name,
+            email: userData.email,
+            surname: userData.family_name,
+
+            dob: new Date(),
+            password: "123456789",
+            username: `${userData.given_name}_${userData.family_name}`,
+          });
+          user = await userToSave.save();
+        }
+        done(null, user);
+      } catch (error) {
+        console.log(error);
+        done(error, null);
+      }
+    }
+  )
+);
+
+google.get(
+  "/auth/google",
+  (req, res, next) => {
+    const redirectUri = req.query.redirect_uri || process.env.FRONTEND_URL;
+    req.session.redirectUri = redirectUri; // Salva il redirect URI nella sessione
+    next();
+  },
+  passport.authenticate("google", { scope: ["email", "profile"] })
+);
+
+google.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/" }),
+  (req, res) => {
+    const user = req.user;
+    const token = {
+      name: user.name,
+      surname: user.surname,
+      email: user.email,
+      _id: user._id,
+      role: user.role,
+    };
+
+    const userToken = jwt.sign(token, process.env.JWT_SECRET);
+    const redirectUri = req.session.redirectUri || process.env.FRONTEND_URL;
+    const finalRedirectUrl = `${redirectUri}?token=${encodeURIComponent(
+      userToken
+    )}`;
+    res.redirect(finalRedirectUrl);
   }
 );
 
--- routes/login/login.js
+++ routes/login/login.js
@@ -1,10 +1,11 @@
-const express = require("express");
+/*const express = require("express");
 const login = express.Router();
 const generateToken = require("../../middleware/generateToken/generateToken");
 const validatePassword = require("../../middleware/validatePassword/validatePassword");
 
 login.post("/login", validatePassword, (req, res) => {
   const user = req.user;
+
   const userToken = generateToken(user);
   res
     .header("Authorization", userToken)
@@ -21,6 +22,51 @@ login.post("/login", validatePassword, (req, res) => {
         _id: user._id,
       },
     });
+});
+
+module.exports = login;*/
+
+const express = require("express");
+const login = express.Router();
+const generateToken = require("../../middleware/generateToken/generateToken");
+const validatePassword = require("../../middleware/validatePassword/validatePassword");
+
+// Login route
+login.post("/login", validatePassword, (req, res) => {
+  try {
+    const user = req.user;
+
+    if (!user) {
+      return res.status(401).send({
+        statusCode: 401,
+        message: "Invalid credentials",
+      });
+    }
+
+    const userToken = generateToken(user);
+
+    res
+      .header("Authorization", userToken)
+      .status(200)
+      .send({
+        statusCode: 200,
+        message: "You are successfully logged in",
+        token: userToken,
+        user: {
+          name: user.name,
+          surname: user.surname,
+          email: user.email,
+          role: user.role,
+          _id: user._id,
+        },
+      });
+  } catch (error) {
+    console.error("Error during login:", error);
+    res.status(500).send({
+      statusCode: 500,
+      message: "Internal server error",
+    });
+  }
 });
 
 module.exports = login;
