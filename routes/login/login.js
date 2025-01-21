3ea14a2b5f9ae41883020435aa0880b1baced82a
uros milenkovic <urosm4471@gmail.com>
2025-01-21 13:05:47 +0100

    fixed

--- routes/login/login.js
+++ routes/login/login.js
@@ -1,4 +1,4 @@
-/*const express = require("express");
+const express = require("express");
 const login = express.Router();
 const generateToken = require("../../middleware/generateToken/generateToken");
 const validatePassword = require("../../middleware/validatePassword/validatePassword");
@@ -22,51 +22,6 @@ login.post("/login", validatePassword, (req, res) => {
         _id: user._id,
       },
     });
-});
-
-module.exports = login;*/
-
-const express = require("express");
-const login = express.Router();
-const generateToken = require("../../middleware/generateToken/generateToken");
-const validatePassword = require("../../middleware/validatePassword/validatePassword");
-
-// Login route
-login.post("/login", validatePassword, (req, res) => {
-  try {
-    const user = req.user;
-
-    if (!user) {
-      return res.status(401).send({
-        statusCode: 401,
-        message: "Invalid credentials",
-      });
-    }
-
-    const userToken = generateToken(user);
-
-    res
-      .header("Authorization", userToken)
-      .status(200)
-      .send({
-        statusCode: 200,
-        message: "You are successfully logged in",
-        token: userToken,
-        user: {
-          name: user.name,
-          surname: user.surname,
-          email: user.email,
-          role: user.role,
-          _id: user._id,
-        },
-      });
-  } catch (error) {
-    console.error("Error during login:", error);
-    res.status(500).send({
-      statusCode: 500,
-      message: "Internal server error",
-    });
-  }
 });
 
 module.exports = login;
